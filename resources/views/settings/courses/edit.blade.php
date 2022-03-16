8d10c826028bfb66108dcc7491c371475eed5f17
noahGms <noah@keshin.net>
2022-03-16 20:30:47 +0100

    Update courses edit view

--- resources/views/settings/courses/edit.blade.php
+++ resources/views/settings/courses/edit.blade.php
@@ -43,7 +43,7 @@
 
             <div class="mt-8">
                 <button role="submit" class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full">
-                    Add
+                    Update
                 </button>
             </div>
         </form>
